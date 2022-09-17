81a5f5ed013767d1af208b0a867f0f8094818018
Maximiliano Delfino <maxidelfino.rr@gmail.com>
2022-09-16 21:35:15 -0300

    BaSP-W05-2022: Ex05 completed and ex04 modified

--- Semana-05/exercises/04_if_else.js
+++ Semana-05/exercises/04_if_else.js
@@ -1,39 +1,36 @@
 console.log('--EXERCISE 4: IF_ELSE');
 
 // a) Create a random number between 0 and 1 using the Math.random() function, if the value is greater than or equal to 0.5 display an alert with the message “Greater than 0.5” and otherwise an alert with the message “Lower than 0.5”.
-console.log('-Exercise 4.A:');
+console.log('-Exercise 4.a:');
 var numberRandom = Math.random();
 if (numberRandom >= 0.5) {
-    console.log('Greater than 0,5');
+    alert('Greater than 0,5');
 } else {
-    console.log('Lower than 0,5');
+    alert('Lower than 0,5');
 }
 
-/*
-b) Create a variable “Age” that contains an integer between 0 and 100 and displays the following alert messages:
+/* b) Create a variable “Age” that contains an integer between 0 and 100 and displays the following alert messages:
     i. “Baby” if the age is less than 2 years;
     ii. “Child” if the age is between 2 and 12 years;
     iii. “Adolescent” between 13 and 19 years old;
     iv. “Young” between 20 and 30 years old;
     v. “Adult” between 31 and 60 years old;
     vi. “Senior Adult” between 61 and 75 years old;
-    vii. “Elderly” if he is older than 75 years.
-*/
-console.log('-Exercise 4.A:');
+    vii. “Elderly” if he is older than 75 years.   */
+console.log('-Exercise 4.b:');
 var age = Math.round( Math.random() * 100);
-console.log(age)
 if (age < 2) {
-    console.log('Baby');
+    alert('Baby');
 } else if (age >= 2 && age <= 12 ){
-    console.log('Child');
+    alert('Child');
 } else if (age >= 13 && age <= 19){
-    console.log('Adolescent');
+    alert('Adolescent');
 } else if (age >= 20 && age <= 30){
-    console.log('Young');
+    alert('Young');
 } else if (age >= 31 && age <= 60){
-    console.log('Adult');
+    alert('Adult');
 } else if (age >= 61 && age <= 75){
-    console.log('Senior Adult');
+    alert('Senior Adult');
 } else if (age >= 75){
-    console.log('Elderly');    
+    alert('Elderly');    
 }
---
+++ Semana-05/exercises/05_for.js
@@ -0,0 +1,36 @@
+console.log('--EXERCISE 5: FOR');
+
+// a) Create an array containing 5 words and iterate through that array using a JavaScript for loop to display an alert using each of the words.
+console.log('-Exercise 5.a:');
+var arr = ['pepe', 'car', 'rocket', 'radium', 'beast'];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// b) To the previous array, convert the first letter of each word to uppercase and show an alert for each modified word.
+console.log('-Exercise 5.b:');
+var newArr = [];
+for (let i = 0; i < arr.length; i++) {
+    arr[i].substring(0,1).toUpperCase();
+    newWord = arr[i].substring(0,1).toUpperCase() + arr[i].substring(1, arr[i].length);
+    newArr.push(newWord);
+}
+for (let i = 0; i < newArr.length; i++) {
+    console.log(newArr[i]);
+}
+
+// c) Create a variable called “sentence” that has an empty string, then the array from point a) go through it with a for loop to save each word inside the sentence variable. At the end show a single alert with the complete chain.
+console.log('-Exercise 5.c:');
+var sentence = '';
+for (let i = 0; i < arr.length; i++) {
+    sentence = sentence + '' + arr[i];
+}
+console.log(sentence); //reemplazar los console.log por alertas
+
+// d) Create an empty array with a for loop of 10 repetitions. Fill the array with the repetition number, that is, at the end of the execution of the for loop there should be 10 elements inside the array, from number 0 to number 9. Show the final array in the browser console (use console .log).
+console.log('-Exercise 5.d:');
+var arr2 = [];
+for (let i = 0; i < 10; i++) {
+    arr2[i] = i;
+}
+console.log(arr2);
