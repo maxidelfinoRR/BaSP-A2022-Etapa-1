3e9225977741795d20d112d8826ca672ce4fa61e
Maximiliano Delfino <maxidelfino.rr@gmail.com>
2022-09-18 18:46:36 -0300

    BaSP-W05-2022: Correction of ex06 and week 04

--- Semana-05/exercises/06_functions.js
+++ Semana-05/exercises/06_functions.js
@@ -11,63 +11,53 @@ console.log(c);
 // b) To the sum function above, add a validation to control if any of the parameters is not a number; if it is not a number, display an alert clarifying that one of the parameters has an error and return the NaN value as the result.
 console.log('-Exercise 6.b:'); 
 function isNum(n){
-    return !isNaN(n);
+    return typeof n === 'number'; 
 }
 function suma2 (a,b) {
     if (isNum(a) && isNum(b)) {
         return a + b;
-    } else {
-        return 'Not a Number (NaN)';
     }
+    return 'Not a Number (NaN)';
 }
-c2 = suma2(3,parseInt(9));
-console.log(c2);
+console.log(suma2(3,'5'));
 
 // c) Separately, create a validate Integer function that receives a number as a parameter and returns true if it is an integer.
 console.log('-Exercise 6.c:'); 
 function validateInteger (a) {
     if (a - Math.floor(a) === 0) {
         return true;
-    } else {
-        return false;
     }
+   return false;
 }
-var a = validateInteger(3);
-console.log(a);
+console.log(validateInteger(3));
 
 // d) To the sum function of exercise 6b) add a call to the function of exercise 6c. and that validates that the numbers are integers. If there are decimals, show an alert with the error and return the number converted to an integer (rounded).
 console.log('-Exercise 6.d:'); 
-function suma3 (a,b) {
+function suma3 (a,b,validate) {
     if (isNum(a) && isNum(b)) {
-        if (validateInteger(a) && validateInteger(b)) {
+        if (validate(a) && validate(b)) {
             return a + b;
-        } else {
-            total = Math.round(a) + Math.round(b);
-            return 'One of the numbers is not an integer, but we will convert it for you :) the total of the sum is: ' + total;
         }
-    } else {
-        return 'Not a Number (NaN)';
+        total = Math.round(a) + Math.round(b);
+        return 'One of the numbers is not an integer, but we will convert it for you :) the total of the sum is: ' + total;
     }
+    return 'Not a Number (NaN)';
 } 
-var c3 = suma3(1,'9');
-console.log(c3);
+console.log(suma3(1,10,validateInteger));
 
 // e) Convert the validation of exercise 6d) into a separate function and call it inside the sum function testing that everything still works the same.
 console.log('-Exercise 6.e:'); 
 function convert(a,b) {
     total = Math.round(a) + Math.round(b);
-        return 'One of the numbers is not an integer, but we will convert it for you :) the total of the sum is: ' + total;
+    return 'One of the numbers is not an integer, but we will convert it for you :) the total of the sum is: ' + total;
 }
-function suma4 (a,b) {
+function suma4 (a,b,validate) {
     if (isNum(a) && isNum(b)) {
-        if (validateInteger(a) && validateInteger(b)) {
+        if (validate(a) && validate(b)) {
             return a + b;
-        } else {
-            return convert(a,b);
         }
-    } else {
-        return 'Not a Number (NaN)';
+        return convert(a,b);
     }
-} 
-var c3 = suma4(1,'9');
-console.log(c3);
+    return 'Not a Number (NaN)';
+}
+console.log( suma4(1,'35',validateInteger));
